42b2a304d4cf1e1ae2a6d210869289f3a8891e03
Ben Hutton <ben.hutton@cinglevue.com>
2017-05-08 17:53:48 +0800

    Added invalid connection test.

--- tests/vpssvr_connectivity_tests.py
+++ tests/vpssvr_connectivity_tests.py
@@ -2,6 +2,14 @@ import unittest
 from mock import patch
 import modules.vps
 
+'''
+Note: These tests require the vpsserver.py server to 
+be started from within the vpssvr server app.
+
+The configuration settings in "configuration.cfg" under
+[vps_server] must be set to connect to the relevant
+server.
+'''
 
 
 class TestConnectivityToVPSServer(unittest.TestCase):
@@ -11,9 +19,11 @@ class TestConnectivityToVPSServer(unittest.TestCase):
         db_connect.return_value.connect.return_value = None
         self.v = modules.vps.VPS()
 
-    def test_make_call_to_vpssvr(self):
+    def test_make_teststatus_call_to_vpssvr(self):
         assert self.v.make_call_to_vpssvr('/vpssvr/api/v1.0/tasks/statustest/1').status_code == 200
 
+    def test_make_invalid_call(self):
+        assert self.v.make_call_to_vpssvr('/vpssvr/api/v1.0/tasks/statu/1').status_code == 404
 
 if __name__ == '__main__':
     unittest.main()
