90c6f4e34586d3c2e631ff046110d93a15d3196f
Ben Hutton <ben.hutton@cinglevue.com>
2017-07-07 07:43:36 +0800

    Added test for viewing VPS plus method for logging in

--- tests/functional_tests/functional_tests.py
+++ tests/functional_tests/functional_tests.py
@@ -1,6 +1,5 @@
 import os
 import tempfile
-
 from selenium import webdriver
 import vpsmanager
 import unittest
@@ -11,21 +10,27 @@ class VPSManagerFunctionalTests(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    #def tearDown(self):
-    #    os.close(self.db_fd)
-    #    os.unlink(vpsmanager.app.config['DATABASE'])
-
-    def test_login_page(self):
-        self.browser.get('http://localhost:3000')
-        assert 'Login - VPS Manager' in self.browser.title
+    def tearDown(self):
         self.browser.quit()
 
-    def test_login(self):
+    def login(self):
         self.browser.get('http://localhost:3000')
         self.browser.find_element_by_id("username").send_keys("ben@benhutton.com.au")
         self.browser.find_element_by_id("password").send_keys("Lijnfe0912")
         self.browser.find_element_by_id("btnSignIn").click()
 
+    def test_login_page(self):
+        self.browser.get('http://localhost:3000')
+        assert 'Login - VPS Manager' in self.browser.title
+
+    def test_login(self):
+        self.login()
+        assert 'VPS Manager' in self.browser.title
+
+    def test_view_vps(self):
+        self.login()
+        self.browser.get('http://localhost:3000/viewVPS?id=878')
+        assert 'View VPS' in self.browser.title
 
 if __name__ == '__main__':
     unittest.main()
