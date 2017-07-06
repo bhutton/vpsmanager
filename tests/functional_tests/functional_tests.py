d8efbd77cc936bbb049a651725c3af913ab10a61
Benjamin Hutton <ben@benhutton.com.au>
2017-07-06 21:47:43 +0800

    Added Login functional test

--- tests/functional_tests/functional_tests.py
+++ tests/functional_tests/functional_tests.py
@@ -1,14 +1,31 @@
+import os
+import tempfile
+
 from selenium import webdriver
-from flask_testing import LiveServerTestCase
+import vpsmanager
+import unittest
+#from flask_testing import LiveServerTestCase
 
-class VPSManagerFunctionalTests(LiveServerTestCase):
+class VPSManagerFunctionalTests(unittest.TestCase):
 
-    def test_home_page(self):
-        browser = webdriver.Firefox()
-        browser.get('http://localhost:3000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-        assert 'Login - VPS Manager' in browser.title
-        browser.quit()
+    #def tearDown(self):
+    #    os.close(self.db_fd)
+    #    os.unlink(vpsmanager.app.config['DATABASE'])
+
+    def test_login_page(self):
+        self.browser.get('http://localhost:3000')
+        assert 'Login - VPS Manager' in self.browser.title
+        self.browser.quit()
+
+    def test_login(self):
+        self.browser.get('http://localhost:3000')
+        self.browser.find_element_by_id("username").send_keys("ben@benhutton.com.au")
+        self.browser.find_element_by_id("password").send_keys("Lijnfe0912")
+        self.browser.find_element_by_id("btnSignIn").click()
+
 
 if __name__ == '__main__':
     unittest.main()
