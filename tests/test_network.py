6b9ee879dd6d9368e30b906267c7b4d5e4644170
Ben Hutton <ben.hutton@cinglevue.com>
2017-07-20 07:54:43 +0800

    Cleanup of VpsmanagerNetworkTestCase

--- tests/test_network.py
+++ tests/test_network.py
@@ -2,22 +2,18 @@ import modules.vps as vps
 from mock import patch, MagicMock
 from tests.test_vpsmanager import VpsmanagerTestCase
 
-expect_value = {'status': 'VPS 878 Updated\n'}
+
 
 class VpsmanagerNetworkTestCase(VpsmanagerTestCase):
 
-    #@patch('modules.vps.VPS.make_call_to_vpssvr')
+    expect_value = {'status': 'VPS 878 Updated\n'}
+
     def test_delete_network_interface(self):
-        #mock_vps.return_value = 'VPS 878 Updated\n'
         v = vps.VPS()
         rv = v.delete_network_interface(1, 878)
-        #assert v.delete_network_interface(1, 878) is 'VPS 878 Updated\n'
-        self.assertDictEqual(rv.json(), expect_value)
+        self.assertDictEqual(rv.json(), self.expect_value)
 
     def test_add_device(self):
-        #mock_vps.return_value = 'VPS 878 Updated\n'
         v = vps.VPS()
         rv = v.addDevice(1, 878, 0)
-        #expect_value = {'status': 'VPS 878 Updated\n'}
-        self.assertDictEqual(rv.json(), expect_value)
-        #assert v.addDevice(1, 878, 0) is 'VPS 878 Updated\n'
+        self.assertDictEqual(rv.json(), self.expect_value)
