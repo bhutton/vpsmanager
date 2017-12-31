19271484c57a1919bd34b1b471dee752fb62d801
Benjamin Hutton <ben@benhutton.com.au>
2017-12-31 21:28:56 +0800

    Moved chrome path into config file.

--- tests/functional_tests/functional_tests.py
+++ tests/functional_tests/functional_tests.py
@@ -1,8 +1,16 @@
 import time
 import warnings
 
+import os, configparser
 from selenium import webdriver
 import unittest
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+test_config = configparser.ConfigParser()
+test_config.read("{}/testing.cfg".format(dir_path))
+
+chrome_path = test_config.get('Chrome', 'executable_path')
+
 
 class VPSManagerFunctionalTests(unittest.TestCase):
 
@@ -11,19 +19,20 @@ class VPSManagerFunctionalTests(unittest.TestCase):
         warnings.filterwarnings("ignore", category=ResourceWarning)
 
         options = webdriver.ChromeOptions()
-        options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+        options.binary_location = chrome_path
         options.add_argument('headless')
         options.add_argument('window-size=1200x600, chrome.verbose=true')
-        self.browser = webdriver.Chrome(chrome_options=options)
+        self.browser = webdriver.Chrome(options=options)
         self.browser.implicitly_wait(10)
-
 
     def tearDown(self):
         self.browser.quit()
 
     def login(self):
         self.browser.get('http://localhost:3000')
+        self.browser.get_screenshot_as_file('login screen.png')
         self.browser.find_element_by_id("username").send_keys("ben@benhutton.com.au")
+        self.browser.get_screenshot_as_file('username.png')
         self.browser.find_element_by_id("password").send_keys("Lijnfe0912")
         self.browser.find_element_by_id("btnSignIn").click()
 
